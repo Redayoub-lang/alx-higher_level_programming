50c15cc2885a48a0bc22e9d8020a21dcde9fc923
Redayoub-lang <redayoub9@gmail.com>
2023-12-07 08:48:26 -0800

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-Returns 0 if the list is empty
-You are not allowed to import any module
-guillaume@ubuntu:~/0x04$ cat 100-main.py
-#!/usr/bin/python3
-weight_average = __import__('100-weight_average').weight_average
+    if len(my_list) == 0:
+        return 0
+    numerator = 0
+    denominator = 0
+    for score, weight in my_list:
+        numerator += score * weight
+        denominator += weight
+    return numerator / denominator
 
-my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
-# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
-result = weight_average(my_list)
-print("Average: {:0.2f}".format(result))
-
-guillaume@ubuntu:~/0x04$ ./100-main.py
-Average: 2.80
-guillaume@ubuntu:~/0x04$ 
